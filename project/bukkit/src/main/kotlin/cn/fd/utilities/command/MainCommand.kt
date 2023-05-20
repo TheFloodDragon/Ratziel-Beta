b5a09245b38361b8c94491173bb5122a2e006a23
MC~蛟龙 <1610105206@qq.com>
2023-05-20 12:10:15 +0800

    小改变

--- project/bukkit/src/main/kotlin/cn/fd/utilities/command/MainCommand.kt
+++ project/bukkit/src/main/kotlin/cn/fd/utilities/command/MainCommand.kt
@@ -1,99 +1,91 @@
 package cn.fd.utilities.command
 
-import cn.fd.utilities.util.KetherParser.eval
 import cn.fd.utilities.util.Loader
-import org.bukkit.command.CommandSender
-import org.bukkit.entity.Player
-import taboolib.common.platform.ProxyCommandSender
-import taboolib.common.platform.command.*
-import taboolib.common.platform.function.adaptCommandSender
-import taboolib.common.platform.function.pluginVersion
-import taboolib.common5.Coerce
-import taboolib.module.kether.parseKetherScript
-import taboolib.module.kether.runKether
+import taboolib.common.platform.Platform
+import taboolib.common.platform.PlatformSide
+import taboolib.common.platform.command.CommandBody
+import taboolib.common.platform.command.CommandHeader
+import taboolib.common.platform.command.subCommand
+import taboolib.common.platform.function.console
 import taboolib.module.lang.sendLang
-import taboolib.platform.util.broadcast
 
-@CommandHeader(
-    name = "fdutilities",
-    aliases = ["fd", "fdu"],
-    description = "FDUtilities插件主命令"
-)
+@Deprecated("测试用")
+@PlatformSide([Platform.BUKKIT])
+@CommandHeader(name = "fdutilities")
 object MainCommand {
-
-    //测试命令
-    @CommandBody(permission = "fdutilities.command.test", permissionDefault = PermissionDefault.TRUE, optional = true)
-    val test = subCommand {
-        execute<CommandSender> { sender, _, argument ->
-            val arg = argument.split("test")[1]
-            if (sender is Player) {
-                val player: Player = sender
-                val msg = player.eval(arg).thenApply { Coerce.toString(it) }.get()
-                player.sendMessage(msg)
-
-                runKether {
-                    arg.parseKetherScript().broadcast()
-                    player.sendMessage(arg.parseKetherScript().toString())
-                }
-
-            }
-
-            //val mapList = PlaceholderAPIExtension.conf.getMapList("TestMap")
-            //info(mapList)
-//            for (map in mapList) {
-//                val c = map["condition"]
-//                info(map)
-//                info(c)
-//                if (sender is Player) {
-//                    val player: Player = sender
-//                    val msg = player.eval(c.toString()).thenApply { Coerce.toString(it) }.get()
-//                    player.sendMessage(msg)
-//
-//                }
-//            }
-
-
-        }
-    }
-
-    /**
-     * 帮助命令
-     */
-    @CommandBody(permission = "fdutilities.command.help", permissionDefault = PermissionDefault.TRUE, optional = true)
-    val help = subCommand {
-        execute<CommandSender> { sender, _, _ ->
-            //显示帮助消息
-            adaptCommandSender(sender).sendLang("Command-Help", pluginVersion)
-        }
-    }
-
-    /**
-     * 主命令
-     */
-    @CommandBody
-    val main = mainCommand {
-        execute<CommandSender> { sender, _, argument ->
-            if (argument.isEmpty()) {
-                //显示帮助消息
-                adaptCommandSender(sender).sendLang("Command-Help", pluginVersion)
-                return@execute
-            }
-        }
-    }
 
     /**
      * 重载插件的命令
      */
-    @CommandBody(optional = true)
-    val reload = subCommand {
-        execute<ProxyCommandSender> { sender, _, _ ->
-            //时间计算
-            val before = System.currentTimeMillis()
-            //重载部分
-            Loader.reloadAll()
-            //发送重载消息
-            sender.sendLang("Plugin-Reload", System.currentTimeMillis() - before)
-        }
+    @CommandBody
+    val main = subCommand {
+        //时间计算
+        val before = System.currentTimeMillis()
+        //重载部分
+        Loader.reloadAll()
+        //发送重载消息
+        console().sendLang("Plugin-Reload", System.currentTimeMillis() - before)
     }
+//
+//    //测试命令
+//    @CommandBody(permission = "fdutilities.command.test", permissionDefault = PermissionDefault.TRUE, optional = true)
+//    val test = subCommand {
+//        execute<CommandSender> { sender, _, argument ->
+//            val arg = argument.split("test")[1]
+//            if (sender is Player) {
+//                val player: Player = sender
+//                val msg = player.eval(arg).thenApply { Coerce.toString(it) }.get()
+//                player.sendMessage(msg)
+//
+//                runKether {
+//                    arg.parseKetherScript().broadcast()
+//                    player.sendMessage(arg.parseKetherScript().toString())
+//                }
+//
+//            }
+//
+//            //val mapList = PlaceholderAPIExtension.conf.getMapList("TestMap")
+//            //info(mapList)
+////            for (map in mapList) {
+////                val c = map["condition"]
+////                info(map)
+////                info(c)
+////                if (sender is Player) {
+////                    val player: Player = sender
+////                    val msg = player.eval(c.toString()).thenApply { Coerce.toString(it) }.get()
+////                    player.sendMessage(msg)
+////
+////                }
+////            }
+//
+//
+//        }
+//    }
+//
+//    /**
+//     * 帮助命令
+//     */
+//    @CommandBody(permission = "fdutilities.command.help", permissionDefault = PermissionDefault.TRUE, optional = true)
+//    val help = subCommand {
+//        execute<CommandSender> { sender, _, _ ->
+//            //显示帮助消息
+//            adaptCommandSender(sender).sendLang("Command-Help", pluginVersion)
+//        }
+//    }
+//
+//    /**
+//     * 主命令
+//     */
+//    @CommandBody
+//    val main = mainCommand {
+//        execute<CommandSender> { sender, _, argument ->
+//            if (argument.isEmpty()) {
+//                //显示帮助消息
+//                adaptCommandSender(sender).sendLang("Command-Help", pluginVersion)
+//                return@execute
+//            }
+//        }
+//    }
+
 
 }
