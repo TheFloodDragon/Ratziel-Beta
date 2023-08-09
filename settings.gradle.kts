86934e268315b49bd96d47c5620ce619745070e3
MC~蛟龙 <1610105206@qq.com>
2023-08-09 20:29:04 +0800

    Try to fix build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,16 @@
 rootProject.name = "FDUtilities"
 
+apply("project")
+apply("script")
+
 //Bukkit
 include("plugin:platform-bukkit")
 //全部聚合版
 include("plugin:all")
 
-apply("project")
-apply("script")
-
 //所有字项目的加载(懒得自己一个一个打)
 fun apply(name: String) {
     File("${rootDir}\\$name").listFiles()?.filter { it.isDirectory }?.forEach {
-        include("$name:${it.name}")
+        include(":$name:${it.name}")
     }
 }
