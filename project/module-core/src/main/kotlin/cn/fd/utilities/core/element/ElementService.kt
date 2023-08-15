dda5173a2c36f612fa90bfcd264c98d8bd5e8335
MC~蛟龙 <1610105206@qq.com>
2023-08-15 10:45:26 +0800

    [A.0.0.1.9-1] Update

--- project/module-core/src/main/kotlin/cn/fd/utilities/core/element/ElementService.kt
+++ project/module-core/src/main/kotlin/cn/fd/utilities/core/element/ElementService.kt
@@ -1,5 +1,7 @@
 package cn.fd.utilities.core.element
 
+import cn.fd.utilities.core.element.parser.ElementHandler
+
 /**
  * ElementService
  *
@@ -7,4 +9,19 @@ package cn.fd.utilities.core.element
  * @since 2023/8/15 9:59
  */
 object ElementService {
+
+    private val registry: HashMap<Array<String>, Array<ElementHandler>> = hashMapOf()
+
+    fun getRegistry(): Map<Array<String>, Array<ElementHandler>> {
+        return registry
+    }
+
+    fun registerElement(et: ElementType) {
+        registry[et.name] = et.handlers
+    }
+
+    fun unregisterElement(at: Array<String>) {
+        registry.remove(at)
+    }
+
 }
