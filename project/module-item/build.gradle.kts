05e4aae1184f18a12d08d0f151836e713b7e1f30
MC~蛟龙 <1610105206@qq.com>
2023-10-21 22:28:58 +0800

    Remove ASM Dependency

--- project/module-item/build.gradle.kts
+++ project/module-item/build.gradle.kts
@@ -1,10 +1,6 @@
 dependencies {
     compileNMS()
     compileCore(12002)
-    // ASM
-    compileOnly("org.ow2.asm:asm:9.4")
-    compileOnly("org.ow2.asm:asm-util:9.4")
-    compileOnly("org.ow2.asm:asm-commons:9.4")
     compileOnly("com.google.guava:guava:32.1.2-jre")
     compileTaboo("platform-bukkit")
     compileTaboo("module-nms")
