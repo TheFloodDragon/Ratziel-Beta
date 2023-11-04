cea6ee866c7c58473bf5f23a7a3fa2ab46470b9b
MC~蛟龙 <1610105206@qq.com>
2023-11-04 11:18:03 +0800

    Try To fix 3

--- project/runtime-bukkit/build.gradle.kts
+++ project/runtime-bukkit/build.gradle.kts
@@ -23,11 +23,9 @@ dependencies {
 
 
 tasks {
+    assemble { dependsOn(tabooRelocateJar) }
     shadowJar {
         relocate("com.tcoded.folialib.", "$rootGroup.library.folia.folialib_0_3_1.")
-    }
-    tabooRelocateJar{
-        dependsOn(shadowJar)
     }
 }
 
