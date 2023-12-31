9c649a78dd985eeddeecd3df88f423fc36d8861a
MC~蛟龙 <1610105206@qq.com>
2023-12-31 22:05:14 +0800

    [A.0.2.3.3] 实验性 | 修复不同形式的ItemStack导致的问题

--- project/module-item/src/main/kotlin/cn/fd/ratziel/module/itemengine/mapping/RefItemStack.kt
+++ project/module-item/src/main/kotlin/cn/fd/ratziel/module/itemengine/mapping/RefItemStack.kt
@@ -1,10 +1,8 @@
 package cn.fd.ratziel.module.itemengine.mapping
 
-import cn.fd.ratziel.core.function.MirrorClass
-import cn.fd.ratziel.core.function.UnsupportedTypeException
-import cn.fd.ratziel.core.function.getFieldUnsafe
-import cn.fd.ratziel.core.function.getMethodUnsafe
+import cn.fd.ratziel.core.function.*
 import cn.fd.ratziel.module.itemengine.nbt.NBTCompound
+import org.bukkit.inventory.ItemStack
 import taboolib.library.reflex.Reflex.Companion.getProperty
 import taboolib.library.reflex.Reflex.Companion.invokeConstructor
 import taboolib.library.reflex.Reflex.Companion.invokeMethod
@@ -19,67 +17,89 @@ import taboolib.module.nms.obcClass
  * @author TheFloodDragon
  * @since 2023/10/28 16:18
  */
-class RefItemStack(protected var data: Any) {
+open class RefItemStack(rawData: Any) {
 
-    // 类型检查
-    init {
-        if (!isOBC() && !isNMS()) throw UnsupportedTypeException(data)
-    }
+    protected open var data: Any =
+        if (isOBC(rawData) || isNMS(rawData)) rawData // CraftItemStack or nms.ItemStack
+        else if (rawData is ItemStack) newObcBK(rawData) // interface bukkit.ItemStack
+        else throw UnsupportedTypeException(rawData) // Unsupported Type
 
     /**
      * 获取物品NBT标签
      */
-    fun getNBT() = this.getAsNMS()?.let { nmsGetTagMethod.get(it) }?.let { NBTCompound(it) }
+    open fun getNBT() = this.getAsNMS()?.let { nmsGetTagMethod.get(it) }?.let { NBTCompound(it) }
 
     /**
      * 设置物品NBT标签
      */
-    fun setNBT(nbt: NBTCompound) = this.getAsNMS()?.let { nmsGetTagMethod.set(it, nbt.getAsNmsNBT()) }
+    open fun setNBT(nbt: NBTCompound) = this.getAsNMS()?.let { nmsGetTagMethod.set(it, nbt.getAsNmsNBT()) }
 
     /**
      * 克隆数据
      */
-    fun clone() = this.apply {
+    open fun clone() = this.apply {
         this.data = if (isOBC()) this.data.invokeMethod("clone")!! else nmsCloneMethod.invoke(this.data)!!
     }
 
     /**
      * 获取NMS形式
+     * CraftItemStack: nms.ItemStack handle
      */
-    fun getAsNMS() = if (isOBC()) data.getProperty("handle") else data
+    open fun getAsNMS() = if (isOBC()) data.getProperty("handle") else data
+
+    /**
+     * 获取OBC形式
+     */
+    open fun getAsOBC() = if (isNMS()) newObc(data) else data
 
     /**
      * 数据类型判断方法
      */
-    fun isOBC() = data::class.java.isAssignableFrom(clazz)
-    fun isNMS() = data::class.java.isAssignableFrom(nmsClass)
+    open fun isOBC() = isOBC(data)
+    open fun isNMS() = isNMS(data)
+
+    protected fun isOBC(target: Any) = target::class.java.isAssignableTo(obcClass)
+    protected fun isNMS(target: Any) = target::class.java.isAssignableTo(nmsClass)
 
     companion object : MirrorClass<RefItemStack>() {
 
-        // org.bukkit.craftbukkit.$VERSION.inventory.CraftItemStack
-        @JvmStatic
+        /**
+         * obc.ItemStack
+         *   org.bukkit.craftbukkit.$VERSION.inventory.CraftItemStack
+         */
         override val clazz by lazy { obcClass("inventory.CraftItemStack") }
 
         @JvmStatic
-        override fun of(obj: Any) = RefItemStack(obj)
+        val obcClass get() = clazz
 
         /**
-         * (nms)ItemStack
+         * nms.ItemStack
          *   1.17+ net.minecraft.world.item.ItemStack
          *   1.17- net.minecraft.server.$VERSION.ItemStack
          */
         @JvmStatic
         val nmsClass by lazy { nmsClass("ItemStack") }
 
-        fun newOBC() = clazz.invokeConstructor()
+        @JvmStatic
+        override fun of(obj: Any) = RefItemStack(obj)
 
-        // private CraftItemStack(net.minecraft.world.item.ItemStack item)
-        fun newOBC(nms: Any) = clazz.invokeConstructor(nms)
+        @JvmStatic
+        fun newObc() = obcClass.invokeConstructor()
 
-        fun newNMS() = nmsClass.invokeConstructor()
+        // private CraftItemStack(nms.ItemStack item)
+        @JvmStatic
+        fun newObc(nms: Any) = obcClass.invokeConstructor(nms)
 
-        // private ItemStack(NBTTagCompound nbt)
-        fun newNMS(nbt: Any) = nmsClass.invokeConstructor(nbt)
+        // private CraftItemStack(bukkit.ItemStack item)
+        @JvmStatic
+        fun newObcBK(bukkit: ItemStack) = obcClass.invokeConstructor(bukkit)
+
+        @JvmStatic
+        fun newNms() = nmsClass.invokeConstructor()
+
+        // private nms.ItemStack(NBTTagCompound nbt)
+        @JvmStatic
+        fun newNms(nbt: Any) = nmsClass.invokeConstructor(nbt)
 
         // private NBTTagCompound v
         // private NBTTagCompound tag
@@ -90,8 +110,8 @@ class RefItemStack(protected var data: Any) {
             )
         }
 
-        // public ItemStack p()
-        // public ItemStack cloneItemStack()
+        // public nms.ItemStack p()
+        // public nms.ItemStack cloneItemStack()
         internal val nmsCloneMethod by lazy {
             ReflexClass.of(nmsClass).structure.getMethodUnsafe(
                 name = if (MinecraftVersion.isUniversal) "p" else "cloneItemStack",
