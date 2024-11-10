1aeacf55c2fd9b658219230c7c75bb81860338ec
MC~蛟龙 <1610105206@qq.com>
2024-11-10 10:42:38 +0800

    Experimental | Dependencies update

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -2,13 +2,13 @@ const val rootName = "Ratziel"
 const val rootGroup = "cn.fd.ratziel"
 const val rootVersion = "A.0.3.1.2"
 
-const val kotlinVersion = "2.0.20"
+const val kotlinVersion = "2.1.0-RC"
 const val serializationVersion = "1.7.2"
 const val coroutineVersion = "1.9.0"
-const val shadowJarVersion = "8.3.0"
-const val taboolibPluginVersion = "2.0.17"
+const val shadowJarVersion = "8.3.5"
+const val taboolibPluginVersion = "2.0.21"
 
-const val taboolibVersion = "6.2.0-beta17"
+const val taboolibVersion = "6.2.0-beta33"
 
 val adventureModules = setOf(
     "net.kyori:adventure-api:4.17.0",
