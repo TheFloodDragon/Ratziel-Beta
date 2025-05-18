8bf064b3e742818344b0c1fe04952e8242fb4519
蛟龙 <1610105206@qq.com>
2025-05-18 10:20:07 +0800

    Experimental | Fix MessageComponentSerializer

--- project/module-common/src/main/kotlin/cn/fd/ratziel/common/message/builder/MessageComponentSerializer.kt
+++ project/module-common/src/main/kotlin/cn/fd/ratziel/common/message/builder/MessageComponentSerializer.kt
@@ -2,12 +2,13 @@ package cn.fd.ratziel.common.message.builder
 
 import cn.fd.ratziel.common.config.Settings
 import cn.fd.ratziel.common.message.Message
-import cn.fd.ratziel.core.serialization.json.isJsonObject
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.JsonDecoder
+import kotlinx.serialization.json.JsonEncoder
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.TextDecoration
 
@@ -22,17 +23,23 @@ object MessageComponentSerializer : KSerializer<Component> {
     override val descriptor = PrimitiveSerialDescriptor("adventure.Component", PrimitiveKind.STRING)
 
     override fun deserialize(decoder: Decoder): Component {
-        val content = decoder.decodeString()
-        return if (content.isJsonObject()) {
-            Message.transformFromJson(content)
-        } else {
-            val component = Message.buildMessage(content)
+        // Json 使用组件构建的形式
+        return if (decoder is JsonDecoder) {
+            val component = Message.buildMessage(decoder.decodeString())
             if (Settings.nonItalic && !component.hasDecoration(TextDecoration.ITALIC)) {
                 component.decoration(TextDecoration.ITALIC, false)
             } else component
+        } else {
+            Message.transformFromJson(decoder.decodeString())
         }
     }
 
-    override fun serialize(encoder: Encoder, value: Component) = encoder.encodeString(Message.transformToJson(value))
+    override fun serialize(encoder: Encoder, value: Component) {
+        if (encoder is JsonEncoder) {
+            encoder.encodeString(Message.wrapper.miniBuilder.serialize(value))
+        } else {
+            encoder.encodeString(Message.transformToJson(value))
+        }
+    }
 
 }
