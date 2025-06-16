3f882ed2896a2dc4a47889cfe9be6f2b66b5bee6
蛟龙 <1610105206@qq.com>
2025-06-16 18:57:26 +0800

    Experimental | 插件使用统计

--- project/runtime-bukkit/src/main/kotlin/cn/fd/ratziel/platform/bukkit/Ratziel.kt
+++ project/runtime-bukkit/src/main/kotlin/cn/fd/ratziel/platform/bukkit/Ratziel.kt
@@ -4,11 +4,17 @@ import taboolib.common.platform.Plugin
 import taboolib.common.platform.function.console
 import taboolib.common.platform.function.pluginVersion
 import taboolib.module.lang.sendLang
+import taboolib.platform.BukkitMetrics
+import taboolib.platform.BukkitPlugin
 
 object Ratziel : Plugin() {
 
     override fun onLoad() {
         console().sendLang("Plugin-Loading", pluginVersion)
+        // 启动插件统计
+        runCatching {
+            BukkitMetrics(BukkitPlugin.getInstance(), "Ratziel", 24631, pluginVersion)
+        }
     }
 
     override fun onEnable() {
